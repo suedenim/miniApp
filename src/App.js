e2b8000dc6c07029b9849eaa64c75ae3b737f18a
suedenim <suedenim@users.noreply.github.com>
2021-06-14 20:29:12 -0700

    added css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-
+import Card from './components/Card';
+import UserInput from './components/UserInput';
 
 function App() {
   return (
     <div>
-
+      <Card> 
+        <UserInput />
+      </Card>
     </div>
   );
 }
